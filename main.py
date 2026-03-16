e7d236fc8ae89cf7a51bcf44cd32fbab23835a68
Fatih Aytekin <42274990+aytechz@users.noreply.github.com>
2026-03-16 04:37:52 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def sort(width, height, length, mass):
+
+    if width <= 0 or height <= 0 or length <= 0 or mass <= 0:
+        return "CHECK DIMENSIONS AND MASS AGAIN. ALL VALUES MUST BE GREATER THAN ZERO."
+    
+    volume = width * height * length
+    bulky = volume >= 1000000 or width >= 150 or height >= 150 or length >= 150
+    heavy = mass >= 20
+    
+    if bulky and heavy:
+        return 'REJECTED'
+    elif bulky or heavy:
+        return 'SPECIAL'
+    else:
+        return 'STANDARD'
